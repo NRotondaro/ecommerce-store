cb777e82c3e61a7d97d03547b6e4a252c9cb5360
NRotondaro <niksonrotondaro1@gmail.com>
2023-08-17 21:42:05 -0300

    feat: add product card

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,6 @@
 import getBillboard from '@/actions/get-billboard';
 import getProducts from '@/actions/get-products';
-import Billboard from '@/components/billboards';
+import Billboard from '@/components/billboard';
 import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 
