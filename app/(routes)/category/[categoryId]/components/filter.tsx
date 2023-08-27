6227cf0067183464d460729fcf5c3bb63aba762a
NRotondaro <niksonrotondaro1@gmail.com>
2023-08-27 16:57:10 -0300

    feat: individual category screen finished

--- app/(routes)/category/[categoryId]/components/filter.tsx
+++ app/(routes)/category/[categoryId]/components/filter.tsx
@@ -4,6 +4,8 @@ import qs from 'query-string';
 
 import { Color, Size } from '@/types';
 import { useRouter, useSearchParams } from 'next/navigation';
+import Button from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 interface FilterProps {
   data: (Size | Color)[];
@@ -40,7 +42,27 @@ const Filter: React.FC<FilterProps> = ({ data, name, valueKey }) => {
     router.push(url);
   };
 
-  return <div>Filter</div>;
+  return (
+    <div className='mb-8'>
+      <h3 className='text-lg font-semibold'>{name}</h3>
+      <hr className='my-4' />
+      <div className='flex flex-wrap gap-2'>
+        {data.map((filter) => (
+          <div key={filter.id} className='flex items-center'>
+            <Button
+              className={cn(
+                'rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300',
+                selectedValue === filter.id && 'bg-black text-white'
+              )}
+              onClick={() => onClick(filter.id)}
+            >
+              {filter.name}
+            </Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
